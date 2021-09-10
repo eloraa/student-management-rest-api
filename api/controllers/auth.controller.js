d64dc314ba97af39aa0bc5daaa35e5f59c1cbf49
Neon <me.neonex@gmail.com>
2021-09-10 19:37:28 +0600

    fix bug: add await for user creation

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -8,7 +8,7 @@ exports.register = async (req, res, next) => {
   try {
     const userData = omit(req.body, 'role');
     const error = await User.validateRoll(userData);
-    if(!error) {
+    if (!error) {
       const user = await new User(userData).save();
       const userTransformed = user.transform();
       res.status(httpStatus.CREATED);
